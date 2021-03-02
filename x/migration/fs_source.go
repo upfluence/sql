42fd4a3edfff5f6216062a186d1a0c1cfa46c97e
Julien Levesy <jlevesy@gmail.com>
2021-03-02 09:15:24 +0100

    x/migration: add NewFSSource

---
+++ x/migration/fs_source.go
@@ -0,0 +1,41 @@
+// +build go1.16
+
+package migration
+
+import (
+	"io/fs"
+
+	"github.com/upfluence/log"
+)
+
+func MustFSSource(fs fs.FS, logger log.Logger) Source {
+	s, err := NewFSSource(fs, logger)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return s
+}
+
+func NewFSSource(src fs.FS, logger log.Logger) (Source, error) {
+	files, err := fs.ReadDir(src, ".")
+
+	if err != nil {
+		return nil, err
+	}
+
+	vs := make([]string, len(files))
+
+	for i, f := range files {
+		vs[i] = f.Name()
+	}
+
+	return NewStaticSource(
+		vs,
+		func(file string) ([]byte, error) {
+			return fs.ReadFile(src, file)
+		},
+		logger,
+	), nil
+}
