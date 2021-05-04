e14a103f61ceb9e2256c23194cae2b1c457bdae6
Alexis Montagne <alexis.montagne@gmail.com>
2021-05-04 10:33:26 -0700

    sqlutil: Add public API to register driver wrapper

--- sqlutil/open_test.go
+++ sqlutil/open_test.go
@@ -1,13 +1,31 @@
 package sqlutil
 
 import (
+	"database/sql"
 	"testing"
+
+	"github.com/lib/pq"
 
 	"github.com/upfluence/sql/backend/postgres"
 )
 
 func TestOpenPostgresDB(t *testing.T) {
 	db, err := Open(WithMaster("postgres", "foobar"))
+
+	if err != nil {
+		t.Errorf("Open() = (_, %+v) wanted nil", err)
+	}
+
+	if !postgres.IsPostgresDB(db) {
+		t.Errorf("invalid wrapping of the DB")
+	}
+}
+
+func TestRegisterDriverWrapper(t *testing.T) {
+	sql.Register("bizbuz", &pq.Driver{})
+	RegisterDriverWrapper("bizbuz", postgres.NewDB)
+
+	db, err := Open(WithMaster("bizbuz", "foobar"))
 
 	if err != nil {
 		t.Errorf("Open() = (_, %+v) wanted nil", err)
