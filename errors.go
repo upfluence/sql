9de471046c6deaee07fd3a270fe4dfe5f4f7eedd
Alexis Montagne <alexis.montagne@gmail.com>
2022-01-11 16:39:54 -0800

    *: Add RollbackError type and add a retry system in ExecuteTx

--- errors.go
+++ errors.go
@@ -18,3 +18,19 @@ type ConstraintError struct {
 func (ce ConstraintError) Error() string {
 	return ce.Cause.Error()
 }
+
+type RollbackType int
+
+const (
+	SerializationFailure RollbackType = iota + 1
+	Locked
+)
+
+type RollbackError struct {
+	Type  RollbackType
+	Cause error
+}
+
+func (re RollbackError) Error() string {
+	return re.Cause.Error()
+}
