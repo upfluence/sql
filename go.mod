0e55db77599bea548e762b8b5cfa67404b375fc9
Alexis Montagne <alexis.montagne@gmail.com>
2022-05-10 17:00:31 -0700

    sqltest: Add MigrationMap to help out storage testing

--- go.mod
+++ go.mod
@@ -5,9 +5,7 @@ go 1.16
 require (
 	github.com/lib/pq v1.4.0
 	github.com/mattn/go-sqlite3 v1.13.1-0.20200416054559-98a44bcf5949
-	github.com/sirupsen/logrus v1.6.0 // indirect
 	github.com/stretchr/testify v1.6.1
 	github.com/upfluence/errors v0.2.2
-	github.com/upfluence/log v0.0.0-20200124211732-c9875854d3b8
-	github.com/upfluence/pkg v1.6.3
+	github.com/upfluence/log v0.0.4
 )
---
+++ sqltest/migration_map.go
@@ -0,0 +1,40 @@
+package sqltest
+
+import (
+	"testing"
+
+	"github.com/upfluence/log/logtest"
+
+	"github.com/upfluence/sql"
+	"github.com/upfluence/sql/x/migration"
+)
+
+type MigrationMap map[string]string
+
+func (mm MigrationMap) fetch(n string) ([]byte, error) {
+	m, ok := mm[n]
+
+	if !ok {
+		return nil, migration.ErrNotExist
+	}
+
+	return []byte(m), nil
+}
+
+func (mm MigrationMap) Source(t testing.TB) migration.Source {
+	var fs = make([]string, 0, len(mm))
+
+	for f := range mm {
+		fs = append(fs, f)
+	}
+
+	return migration.NewStaticSource(
+		fs,
+		migration.StaticFetcher(mm.fetch),
+		logtest.WrapTestingLogger(t),
+	)
+}
+
+func (mm MigrationMap) Migrator(t testing.TB, db sql.DB) migration.Migrator {
+	return migration.NewMigrator(db, mm.Source(t))
+}
